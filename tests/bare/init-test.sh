a9a6e79773138a14868ea833e38b6332ca87e330
francescobianco <info.francescobianco@gmail.com>
2025-03-19 22:34:29 +0100

    test

--- tests/bare/init-test.sh
+++ tests/bare/init-test.sh
@@ -2,6 +2,13 @@
 
 echo "===> Test: init-test"
 
+rm -f tests/fixtures/fresh-local/.gitpopper
+rm -fr tests/fixtures/fresh-remote
+git add .
+git commit -am "test"
+git push
+
+
 cd tests/fixtures/fresh-local || exit 1
 rm -f .gitpopper
 
